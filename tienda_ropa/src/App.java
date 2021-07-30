f0c41c9828fe8d46a93954d7593b98706334e7cb
CaroAlexander <82767588+CaroAlexander@users.noreply.github.com>
2021-07-30 17:55:20 -0500

    Se creo poryecto Java para la tienda de ropa

---
+++ tienda_ropa/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("MI TIENDA DE ROPA");
+
+        Producto productoUno = new Producto();
+
+        productoUno.setNombre("Camisa");
+        productoUno.setPrecio(65000.0);
+
+        productoUno.mostrar();
+    }
+}
